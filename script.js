3fad69738541eb152ad09bf5a77e6a79bae8b69a
WestMan777 <ethanwestevans@gmail.com>
2024-07-26 18:42:57 -0400

    adds query selectors to all the buttons on the calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+const viewport = document.querySelector('viewport');
+
+const divideButton = document.querySelector('divide-button');
+const multiplyButton = documnet.querySelector('multiply-button');
+const plusButton = documnet.querySelector('plus-button');
+const minusButton = documnet.querySelector('minus-button');
+const clearButton = documnet.querySelector('clear-button');
+const equalsButton = documnet.querySelector('equals-button');
+const negativeButton = documnet.querySelector('negative-button');
+
+const leftParenthesesButton = document.querySelector('left-parentheses-button');
+const rightParenthesesButton = document.querySelector('right-parentheses-button');
+
+const oneButton = documnet.querySelector('one-button');
+const twoButton = documnet.querySelector('two-button');
+const threeButton = documnet.querySelector('three-button');
+const fourButton = documnet.querySelector('four-button');
+const fiveButton = documnet.querySelector('five-button');
+const sixButton = documnet.querySelector('six-button');
+const sevenButton = documnet.querySelector('seven-button');
+const eightButton = documnet.querySelector('eight-button');
+const nineButton = documnet.querySelector('nine-button');
+const zeroButton = documnet.querySelector('zero-button');
